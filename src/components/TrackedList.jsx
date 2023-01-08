2b2a39205ae2d6e05a400a54b5eceb5b7994cdd6
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-08 05:50:38 -0800

    commit

--- src/components/TrackedList.jsx
+++ src/components/TrackedList.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import TrackerCard from './TrackerCard'
+import { data } from '../constants'
 
 function TrackedList() {
+  // function setTimeframe() {
+  //   if() {
+      
+  //   }
+  // }
+  const cardList = data.trackedData.map((data, i) => (
+    <TrackerCard
+      key={data.title}
+      title={data.title}
+    />
+  ));
   return (
     <div>
       <TrackerCard />
